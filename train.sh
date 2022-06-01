989cadebc5caaf2a65d8e8ff3d7456ba43126de2
yjh0410 <1394571815@qq.com>
2022-06-01 11:22:02 +0800

    update

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 python train.py \
         --cuda \
         -d ucf24 \
-        -v baseline \
+        -v yowof \
         --num_workers 4 \
         -p ./weights/pretrained/yolof-rt-R18/yolof-rt-R18_29.2.pth
