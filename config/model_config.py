3925d5ad32ade7a4de41e44afb6569372124910f
yjh0410 <1394571815@qq.com>
2022-06-01 11:23:24 +0800

    update

--- config/model_config.py
+++ config/model_config.py
@@ -68,7 +68,8 @@ model_config = {
 
     'yowof-r18': {
         # input
-        'img_size': 320,
+        'train_size': 320,
+        'test_size': 320,
         'len_clip': 7,
         'format': 'RGB',
         'pixel_mean': [123.675, 116.28, 103.53],  # imagenet pixel mean
