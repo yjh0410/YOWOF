87c5bd37e62c25ab0da276c5b6a5bec24db324a9
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-26 17:08:20 +0800

    train yowof-r18 on jhmdb

--- train.py
+++ train.py
@@ -151,57 +151,6 @@ def train():
         if args.distributed:
             dataloader.batch_sampler.sampler.set_epoch(epoch)            
 
-        # evaluation
-        if (epoch) % args.eval_epoch == 0 or (epoch + 1) == max_epoch:
-            # check evaluator
-            if distributed_utils.is_main_process():
-                if evaluator is None:
-                    print('No evaluator ... save model and go on training.')
-                    print('Saving state, epoch: {}'.format(epoch + 1))
-                    weight_name = '{}_epoch_{}.pth'.format(args.version, epoch + 1)
-                    checkpoint_path = os.path.join(path_to_save, weight_name)
-                    torch.save({'model': model_without_ddp.state_dict(),
-                                'optimizer': optimizer.state_dict(),
-                                'lr_scheduler': lr_scheduler.state_dict(),
-                                'epoch': epoch,
-                                'args': args}, 
-                                checkpoint_path)                      
-                    
-                else:
-                    print('eval ...')
-                    # set eval mode
-                    model_without_ddp.trainable = False
-                    model_without_ddp.stream_infernce = True
-                    model_without_ddp.eval()
-
-                    # evaluate
-                    evaluator.evaluate(model_without_ddp)
-
-                    cur_frame_map = evaluator.frame_map
-                    if cur_frame_map > best_frame_map:
-                        # update best-map
-                        best_frame_map = cur_frame_map
-                        # save model
-                        print('Saving state, epoch:', epoch + 1)
-                        weight_name = '{}_epoch_{}_{:.2f}.pth'.format(args.version, epoch + 1, best_frame_map*100)
-                        checkpoint_path = os.path.join(path_to_save, weight_name)
-                        torch.save({'model': model_without_ddp.state_dict(),
-                                    'optimizer': optimizer.state_dict(),
-                                    'lr_scheduler': lr_scheduler.state_dict(),
-                                    'epoch': epoch,
-                                    'args': args}, 
-                                    checkpoint_path)                      
-
-                    # set train mode.
-                    model_without_ddp.trainable = True
-                    model_without_ddp.stream_infernce = False
-                    model_without_ddp.train()
-        
-            if args.distributed:
-                # wait for all processes to synchronize
-                dist.barrier()
-
-
         # train one epoch
         for iter_i, (video_clips, targets) in enumerate(dataloader):
             ni = iter_i + epoch * epoch_size
@@ -288,6 +237,7 @@ def train():
                     print('eval ...')
                     # set eval mode
                     model_without_ddp.trainable = False
+                    model_without_ddp.stream_infernce = True
                     model_without_ddp.eval()
 
                     # evaluate
@@ -310,6 +260,7 @@ def train():
 
                     # set train mode.
                     model_without_ddp.trainable = True
+                    model_without_ddp.stream_infernce = False
                     model_without_ddp.train()
         
             if args.distributed:
