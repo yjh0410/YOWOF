bb4901da826622470403159643bcbf80d0699bf2
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-27 15:54:06 +0800

    new yowof network

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@ python train.py \
         -d jhmdb \
         -v yowof-r18 \
         --num_workers 4 \
-        --eval_epoch 2 \
+        --eval_epoch 100 \
         -p ./weights/pretrained/yolof-rt-R18/yolof-rt-R18_29.2.pth
