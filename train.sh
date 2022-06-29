70baa582134ac6674c6540b359d809b4888e8993
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-29 19:51:41 +0800

    train yowof-r50 on jhmdb with amp

--- train.sh
+++ train.sh
@@ -1,16 +1,16 @@
-python train.py \
-        --cuda \
-        -d jhmdb \
-        -v yowof-r18 \
-        --num_workers 4 \
-        --eval_epoch 2 \
-        -p ./weights/pretrained/yolof-rt-R18/yolof-rt-R18_29.2.pth
-
 # python train.py \
 #         --cuda \
 #         -d jhmdb \
-#         -v yowof-r50 \
+#         -v yowof-r18 \
 #         --num_workers 4 \
 #         --eval_epoch 2 \
-#         --fp16 \
-#         -p ./weights/pretrained/yolof-rt-R50/yolof-rt-R50_33.9.pth
+#         -p ./weights/pretrained/yolof-rt-R18/yolof-rt-R18_29.2.pth
+
+python train.py \
+        --cuda \
+        -d jhmdb \
+        -v yowof-r50 \
+        --num_workers 4 \
+        --eval_epoch 2 \
+        --fp16 \
+        -p ./weights/pretrained/yolof-rt-R50/yolof-rt-R50_33.9.pth
