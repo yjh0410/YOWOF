001e87458c90b97eb52546b6b7e6058d2b81e913
yjh0410 <1394571815@qq.com>
2022-07-01 14:16:09 +0800

    train yowof-r18 on jhmdb with amp

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@ python train.py \
         -v yowof-r18 \
         --num_workers 4 \
         --eval_epoch 2 \
-        --fp 16 \
+        --fp16 \
         -p ./weights/pretrained/yolof-rt-R18/yolof-rt-R18_29.2.pth
 
 # python train.py \
