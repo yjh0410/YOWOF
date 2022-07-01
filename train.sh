9126bc2d18174e89c3603c5cc4fe24ddb21c7900
yjh0410 <1394571815@qq.com>
2022-07-01 12:24:53 +0800

    train yowof-r18 on jhmdb with amp

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ python train.py \
         --num_workers 4 \
         --eval_epoch 2 \
         --fp16 \
-#         -p ./weights/pretrained/yolof-rt-R18/yolof-rt-R18_29.2.pth
+        -p ./weights/pretrained/yolof-rt-R18/yolof-rt-R18_29.2.pth
 
 # python train.py \
 #         --cuda \
