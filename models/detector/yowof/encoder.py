d7dc80d1afa7452d5b4a1da3a90744a990e4cf78
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-07-12 17:06:05 +0800

    update force

--- models/detector/yowof/encoder.py
+++ models/detector/yowof/encoder.py
@@ -48,34 +48,60 @@ class CSAM(nn.Module):
         return out
 
 
-class TemporalGroupConv(nn.Module):
-    def __init__(self, in_dim, in_len_clip=1, out_len_clip=1):
+# Spatial Self Attetion Module
+class SSAM(nn.Module):
+    def __init__(self, in_dim, dropout=0.):
         super().__init__()
-        self.group_convs = nn.ModuleList([
-            nn.Sequential(
-                Conv(in_dim * in_len_clip, in_dim, k=1, g=in_dim, act_type=None, norm_type=None),
-                Conv(in_dim, in_dim, k=3, p=1, act_type='relu', norm_type='BN')
-            )
-            for _ in range(out_len_clip)
-        ])
+        self.in_dim = in_dim
+        self.scale = in_dim ** -0.5
+
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+        # attention
+        self.attend = nn.Softmax(dim = -1)
+        self.ffn = nn.Sequential(
+            nn.Conv2d(in_dim, in_dim*4, kernel_size=1),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(dropout),
+            nn.Conv2d(in_dim*4, in_dim, kernel_size=1),
+            nn.Dropout2d(dropout)
+        )
+        self.norm = nn.InstanceNorm2d(in_dim)
 
     def forward(self, x):
         """
-            x: [B, CK, H, W]
+            x: (Tensor) [B, C, H, W]
         """
-        feats = [layer(x) for layer in self.group_convs]
+        B, C, H, W = x.size()
 
-        return feats
+        # query: [B, N, C]
+        q = x.view(B, C, -1).transpose(-1, -2)
+        # key: [B, C, N]
+        k = x.view(B, C, -1) 
+        # value: [B, N, C]
+        v = x.view(B, C, -1).transpose(-1, -2)
+
+        # self attention: [B, N, N]
+        attn = self.attend(torch.matmul(q, k) * self.scale)
+        out = torch.matmul(attn, v)
+
+        # [B, N, C] -> [B, C, N] -> [B, C, H, W]
+        out = out.transpose(-1, -2).view(B, C, H, W)
+
+        # output
+        out = self.ffn(out) * self.gamma + x
+
+        return out
 
 
-class FuseConv(nn.Module):
-    def __init__(self, in_dim, N=1, dropout=0.):
+class ChannelFuseConv(nn.Module):
+    def __init__(self, c1, c2, dropout=0.):
         super().__init__()
         self.fuse_convs = nn.Sequential(
-            Conv(in_dim*N, in_dim, k=1, act_type='relu', norm_type='BN'),
-            Conv(in_dim, in_dim, k=3, p=1, act_type='relu', norm_type='BN'),
-            CSAM(in_dim, dropout),
-            Conv(in_dim, in_dim, k=3, p=1, act_type='relu', norm_type='BN')
+            Conv(c1 + c2, c2, k=1, act_type='relu', norm_type='BN'),
+            Conv(c2, c2, k=3, p=1, act_type='relu', norm_type='BN'),
+            CSAM(c2, dropout),
+            Conv(c2, c2, k=3, p=1, act_type='relu', norm_type='BN')
         )
 
     def forward(self, x):
@@ -88,30 +114,12 @@ class FuseConv(nn.Module):
         return x
 
 
-class STCEncoder(nn.Module):
-    def __init__(self, in_dim, out_dim, len_clip=1, depth=1, dropout=0.):
+class TemporalShuffle(nn.Module):
+    def __init__(self):
         super().__init__()
-        self.in_dim = in_dim
-        self.len_clip = len_clip
-
-        # input proj
-        self.input_proj = nn.ModuleList([
-            nn.Conv2d(in_dim, out_dim, kernel_size=1)
-            for _ in range(len_clip)
-            ])
-
-        self.temp_group_convs = nn.ModuleList([
-            TemporalGroupConv(out_dim, len_clip // 2**(i), len_clip // 2**(i+1))
-            for i in range(int(math.log2(len_clip)))
-        ])
-
-        self.channel_fuse_convs = nn.ModuleList([
-            FuseConv(out_dim, N=len_clip // 2**(i+1) + 1, dropout=dropout)
-            for i in range(int(math.log2(len_clip)))
-        ])
 
 
-    def temporal_shuffle(self, feats):
+    def forward(self, feats):
         """
         Inputs:
             feats: (List) [K, B, C, H, W]
@@ -129,7 +137,12 @@ class STCEncoder(nn.Module):
         return x
 
 
-    def temporal_unshuffle(self, feats):
+class TemporalUnShuffle(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+    def forward(self, feats):
         """
         Inputs:
             feats: (Tensor) [B, CK, H, W]
@@ -151,20 +164,62 @@ class STCEncoder(nn.Module):
         return x
 
 
+# STC Encoder
+class STCEncoder(nn.Module):
+    def __init__(self, in_dim, out_dim, len_clip=1, depth=1, dropout=0.):
+        """
+            in_dim: (Int) -> dim of single feature
+            len_clip: (Int) -> length of video clip
+        """
+        super().__init__()
+        self.in_dim = in_dim
+        self.out_dim = out_dim
+        self.len_clip = len_clip
+
+        # input proj
+        self.input_proj = nn.ModuleList([
+            nn.Conv2d(in_dim, out_dim, kernel_size=1)
+            for _ in range(len_clip)
+            ])
+
+        # input proj
+        self.temporal_shuffle = nn.ModuleList([
+            nn.Sequential(
+                TemporalShuffle(),
+                Conv(out_dim * len_clip, out_dim, k=1, g=out_dim, act_type=None, norm_type=None),
+                Conv(out_dim, out_dim, k=3, p=1, act_type=None, norm_type='BN')
+            )
+        ])
+
+        # CSAM
+        self.csam = nn.ModuleList([CSAM(out_dim, dropout) for _ in range(depth)])
+
+        # SSAM
+        self.ssam = nn.ModuleList([SSAM(out_dim, dropout) for _ in range(depth)])
+
+
+        self.channel_fuse_convs = nn.ModuleList([
+            ChannelFuseConv(out_dim, out_dim, dropout=dropout)
+            for _ in range(depth)
+        ])
+
+
     def forward(self, feats):
         """
             feats: (List) [K, B, C, H, W]
         """
-        # print(feats[0].shape)
-        feats = [layer(feat) for feat, layer in zip(feats, self.input_proj)]
-        kf_feat = feats[-1]
+        # input proj
+        feats = [layer(x) for x, layer in zip(feats, self.input_proj)]
 
-        for tgconv, cfconv in zip(self.temp_group_convs, self.channel_fuse_convs):
-            # [B, K, C, H, W] -> [B, CK, H, W]
-            x = self.temporal_shuffle(feats)
-            # List[K, B, C, H, W]
-            feats = tgconv(x)
-            kf_feat = torch.cat([kf_feat, *feats], dim=1)
-            kf_feat = cfconv(kf_feat)
+        for ts, ssam, csam, fuse in zip(self.temporal_shuffle, self.ssam, self.csam, self.channel_fuse_convs):
+            kf_feat = feats[-1]
+            # temporal shuffle: [B, C, H, W]
+            x = ts(feats)
+
+            # SSAM & CSAM
+            x = ssam(x)
+            x = csam(x)
+            # channel fuse
+            kf_feat = fuse(torch.cat([kf_feat, x], dim=1))
 
         return kf_feat
