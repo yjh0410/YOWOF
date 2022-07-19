144f0b132eb3d2e0a95d12cbddeee5e0d13a121f
yjh0410 <1394571815@qq.com>
2022-07-19 19:40:24 +0800

    train convlstm on ucf24

--- train_jhmdb.sh
+++ train_jhmdb.sh
@@ -4,7 +4,7 @@ python train.py \
         -d jhmdb \
         -v yowof-r18 \
         --num_workers 4 \
-        --eval_epoch 1 \
+        --eval_epoch 2 \
         --fp16 \
         --eval \
         # -p ./weights/pretrained/yolof-rt-R18/yolof-rt-R18_29.2.pth
--- train_ucf.sh
+++ train_ucf.sh
@@ -4,9 +4,9 @@ python train.py \
         -d ucf24 \
         -v yowof-r18 \
         --num_workers 4 \
-        --eval \
         --eval_epoch 2 \
         --fp16 \
+        --eval \
         # --ema \
         # -p ./weights/pretrained/yolof-rt-R18/yolof-rt-R18_29.2.pth
 
