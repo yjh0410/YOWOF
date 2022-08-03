a7ebb2ffaf883f5f15c31ecd08c8832d5bef18c2
yjh0410 <1394571815@qq.com>
2022-08-03 19:08:18 +0800

    train yowof-r18 on jhmdb with K16

--- config/model_config.py
+++ config/model_config.py
@@ -4,8 +4,8 @@
 model_config = {
     'yowof-r18': {
         # input
-        'train_size': 320,
-        'test_size': 320,
+        'train_size': 224,
+        'test_size': 224,
         'format': 'RGB',
         'pixel_mean': [123.675, 116.28, 103.53],  # imagenet pixel mean
         'pixel_std': [58.395, 57.12, 57.375],     # imagenet pixel std
@@ -37,14 +37,13 @@ model_config = {
         'num_reg_heads': 4,
         'head_depthwise': False,
         # post process
-        'conf_thresh': 0.15,
+        'conf_thresh': 0.1,
         'nms_thresh': 0.5,
         # anchor box
         'anchor_size': [[16, 16],
                         [32, 32], 
                         [64, 64], 
-                        [128, 128],
-                        [256, 256]],
+                        [128, 128]],
         # matcher
         'matcher': 'uniform_matcher',
         'topk': 4,
