47a8dc655d0b693f10392231b25cd7c56ad34cbf
yjh0410 <1394571815@qq.com>
2022-08-15 19:15:23 +0800

    update README

--- eval_ucf.sh
+++ eval_ucf.sh
@@ -6,6 +6,6 @@ python eval.py \
         --gt_folder ./evaluator/groundtruths_ucf_jhmdb/groundtruths_ucf/ \
         --dt_folder ./results/ucf_detections/yowof-r18/detections_1/ \
         --save_path ./evaluator/eval_results/ \
-        --weight ./weights/ucf24/yowof-r18/yowof-r18_c80.5.pth \
+        --weight ./weights/ucf24/yowof-r18/yowof-r18_epoch_1_92.5_94.8.pth \
         --cal_mAP \
         --redo \
