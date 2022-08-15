188f23b0c8aff9009557e4a3e6db6c70eae9eda2
yjh0410 <1394571815@qq.com>
2022-08-15 15:15:50 +0800

    train yowof-r18 on ucf24

--- train.py
+++ train.py
@@ -208,52 +208,6 @@ def train():
         if args.distributed:
             dataloader.batch_sampler.sampler.set_epoch(epoch)            
 
-        # evaluation
-        if (epoch) % args.eval_epoch == 0 or (epoch + 1) == max_epoch:
-            # check evaluator
-            model_eval = ema.ema if args.ema else model_without_ddp
-            if distributed_utils.is_main_process():
-                if evaluator is None:
-                    print('No evaluator ... save model and go on training.')
-                    print('Saving state, epoch: {}'.format(epoch + 1))
-                    weight_name = '{}_epoch_{}.pth'.format(args.version, epoch + 1)
-                    checkpoint_path = os.path.join(path_to_save, weight_name)
-                    torch.save({'model': model_eval.state_dict(),
-                                # 'optimizer': optimizer.state_dict(),
-                                # 'lr_scheduler': lr_scheduler.state_dict(),
-                                'epoch': epoch,
-                                'args': args}, 
-                                checkpoint_path)                      
-                    
-                else:
-                    print('eval ...')
-                    # set eval mode
-                    model_eval.trainable = False
-                    model_eval.eval()
-
-                    # evaluate
-                    accu, recall, _ = evaluator.evaluate_accu_recall(model_eval, epoch + 1)
-
-                    # save model
-                    print('Saving state, epoch:', epoch + 1)
-                    weight_name = '{}_epoch_{}_{:.1f}_{:.1f}.pth'.format(args.version, epoch+1, accu*100, recall*100)
-                    checkpoint_path = os.path.join(path_to_save, weight_name)
-                    torch.save({'model': model_eval.state_dict(),
-                                # 'optimizer': optimizer.state_dict(),
-                                # 'lr_scheduler': lr_scheduler.state_dict(),
-                                'epoch': epoch,
-                                'args': args}, 
-                                checkpoint_path)                      
-
-                    # set train mode.
-                    model_eval.trainable = True
-                    model_eval.train()
-        
-            if args.distributed:
-                # wait for all processes to synchronize
-                dist.barrier()
-
-
         # train one epoch
         for iter_i, (frame_ids, video_clips, targets) in enumerate(dataloader):
             ni = iter_i + epoch * epoch_size
