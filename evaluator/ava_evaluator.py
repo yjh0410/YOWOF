4100e4f3b74babecffe154db4e87a261c0e22489
yjh0410 <1394571815@qq.com>
2022-09-05 09:31:18 +0800

    debug eval on ava_v2.2

--- evaluator/ava_evaluator.py
+++ evaluator/ava_evaluator.py
@@ -230,12 +230,12 @@ class AVA_Evaluator(object):
                 sec = key_frame_info[1]
 
                 # [[x1, y1, x2, y2, cls_out, [video_idx, sec]], ...]
-                preds_list = [[bbox[:4].tolist(), bbox[4:], [video_idx, sec]] for bbox in bboxes]
-                # for bbox in bboxes:
-                #     x1, y1, x2, y2 = bbox[:4]
-                #     cls_out = bbox[4:]
+                # preds_list = [[bbox[:4].tolist(), bbox[4:], [video_idx, sec]] for bbox in bboxes]
+                for bbox in bboxes:
+                    x1, y1, x2, y2 = bbox[:4]
+                    cls_out = bbox[4:]
 
-                #     preds_list.append([[x1,y1,x2,y2], cls_out, [video_idx, sec]])
+                    preds_list.append([[x1,y1,x2,y2], cls_out, [video_idx, sec]])
 
             self.update_stats(preds_list)
             if iter_i % 1 == 0:
