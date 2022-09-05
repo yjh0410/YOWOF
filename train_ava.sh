1b16c92d52057f36763d9d6934857cb54ca9e2d6
yjh0410 <1394571815@qq.com>
2022-09-05 14:24:22 +0800

    debug eval on ava_v2.2

--- train_ava.sh
+++ train_ava.sh
@@ -3,7 +3,7 @@ python train.py \
         --cuda \
         -d ava_v2.2 \
         -v yowof-r18 \
-        --num_workers 4 \
+        --num_workers 8 \
         --eval_epoch 1 \
         --eval \
         # --fp16 \
