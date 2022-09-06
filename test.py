e5e39abeb406b09262de566bc1b0553536a7638b
yjh0410 <1394571815@qq.com>
2022-09-06 11:14:26 +0800

    update

--- test.py
+++ test.py
@@ -77,6 +77,7 @@ def inference_ucf_jhmdb(args, d_cfg, model, device, dataset, class_names=None, c
 
         if frame_id[:-10] != prev_frame_id:
             # a new video
+            print(prev_frame_id)
             prev_frame_id = frame_id[:-10]
             model.initialization = True
 
