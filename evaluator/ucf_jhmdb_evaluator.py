1c5dd860cf56176cb91ce869b6d1ee04f218082d
yjh0410 <1394571815@qq.com>
2022-09-10 11:13:26 +0800

    update README & add demo.py

--- evaluator/ucf_jhmdb_evaluator.py
+++ evaluator/ucf_jhmdb_evaluator.py
@@ -63,7 +63,7 @@ class UCF_JHMDB_Evaluator(object):
 
 
     @torch.no_grad()
-    def evaluate_accu_recall_stream(self, model, epoch=1):
+    def evaluate_accu_recall(self, model, epoch=1):
         # number of groundtruth
         total_num_gts = 0
         proposals   = 0.0
@@ -182,128 +182,6 @@ class UCF_JHMDB_Evaluator(object):
                     log_info = "[%d / %d] precision: %f, recall: %f, fscore: %f" % (iter_i, epoch_size, precision, recall, fscore)
                     print(log_info, flush=True)
                 break
-
-        classification_accuracy = 1.0 * correct_classification / (total_detected + eps)
-        locolization_recall = 1.0 * total_detected / (total_num_gts + eps)
-
-        print("Classification accuracy: %.3f" % classification_accuracy)
-        print("Locolization recall: %.3f" % locolization_recall)
-
-        return classification_accuracy, locolization_recall, current_dir
-
-
-    @torch.no_grad()
-    def evaluate_accu_recall(self, model, epoch=1):
-        # number of groundtruth
-        total_num_gts = 0
-        proposals   = 0.0
-        correct     = 0.0
-        fscore = 0.0
-
-        correct_classification = 0.0
-        total_detected = 0.0
-        eps = 1e-5
-
-        epoch_size = len(self.testloader)
-
-        # initalize model
-        model.set_inference_mode(mode='clip')
-
-        # inference
-        for iter_i, (batch_frame_id, batch_video_clip, batch_target) in enumerate(self.testloader):
-
-            # prepare
-            batch_video_clip = batch_video_clip.to(self.device) # [B, T, 3, H, W], B=1
-
-            with torch.no_grad():
-                # inference
-                batch_output = model(batch_video_clip)
-
-                # process batch
-                for bi in range(len(batch_output)):
-                    frame_id = batch_frame_id[bi]
-                    output = batch_output[bi]
-                    target = batch_target[bi]
-                    scores, labels, bboxes = output
-
-                    # rescale bbox
-                    orig_size = target['orig_size'].tolist()
-                    bboxes = rescale_bboxes(bboxes, orig_size)
-
-                    if not os.path.exists('results'):
-                        os.mkdir('results')
-
-                    if self.dataset == 'ucf24':
-                        detection_path = os.path.join('results', 'ucf_detections', self.model_name, 'detections_' + str(epoch), frame_id)
-                        current_dir = os.path.join('results', 'ucf_detections',  self.model_name, 'detections_' + str(epoch))
-                        os.makedirs(current_dir, exist_ok=True)
-
-                    else:
-                        detection_path = os.path.join('results', 'jhmdb_detections',  self.model_name, 'detections_' + str(epoch), frame_id)
-                        current_dir = os.path.join('results', 'jhmdb_detections',  self.model_name, 'detections_' + str(epoch))
-                        os.makedirs(current_dir, exist_ok=True)
-
-                    with open(detection_path, 'w+') as f_detect:
-                        for score, label, bbox in zip(scores, labels, bboxes):
-                            x1 = round(bbox[0])
-                            y1 = round(bbox[1])
-                            x2 = round(bbox[2])
-                            y2 = round(bbox[3])
-                            cls_id = int(label) + 1
-
-                            f_detect.write(
-                                str(cls_id) + ' ' + str(score) + ' ' \
-                                    + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + '\n')
-
-                    tgt_bboxes = target['boxes'].numpy()
-                    tgt_labels = target['labels'].numpy()
-                    ow, oh = target['orig_size'].tolist()
-                    num_gts = tgt_bboxes.shape[0]
-
-                    # count number of total groundtruth
-                    total_num_gts += num_gts
-
-                    pred_list = [] # LIST OF CONFIDENT BOX INDICES
-                    for i in range(len(scores)):
-                        score = scores[i]
-                        if score > self.conf_thresh:
-                            proposals += 1
-                            pred_list.append(i)
-
-                    for i in range(num_gts):
-                        tgt_bbox = tgt_bboxes[i]
-                        tgt_label = tgt_labels[i]
-                        # rescale groundtruth bbox
-                        tgt_bbox[[0, 2]] *= ow
-                        tgt_bbox[[1, 3]] *= oh
-
-                        tgt_x1, tgt_y1, tgt_x2, tgt_y2 = tgt_bbox
-                        box_gt = [tgt_x1, tgt_y1, tgt_x2, tgt_y2, 1.0, 1.0, tgt_label]
-                        best_iou = 0
-                        best_j = -1
-
-                        for j in pred_list: # ITERATE THROUGH ONLY CONFIDENT BOXES
-                            iou = bbox_iou(box_gt, bboxes[j], x1y1x2y2=True)
-                            if iou > best_iou:
-                                best_j = j
-                                best_iou = iou
-
-                        if best_iou > self.iou_thresh:
-                            total_detected += 1
-                            # print(labels[best_j], tgt_label)
-                            if int(labels[best_j]) == int(tgt_label):
-                                correct_classification += 1
-
-                        if best_iou > self.iou_thresh and int(labels[best_j]) == int(tgt_label):
-                            correct += 1
-
-                precision = 1.0 * correct / (proposals + eps)
-                recall = 1.0 * correct / (total_num_gts + eps)
-                fscore = 2.0 * precision * recall / (precision + recall + eps)
-
-                if iter_i % 500 == 0:
-                    log_info = "[%d / %d] precision: %f, recall: %f, fscore: %f" % (iter_i, epoch_size, precision, recall, fscore)
-                    print(log_info, flush=True)
 
         classification_accuracy = 1.0 * correct_classification / (total_detected + eps)
         locolization_recall = 1.0 * total_detected / (total_num_gts + eps)
