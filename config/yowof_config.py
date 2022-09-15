4b182ee56a5aae02afcfdf144e7e660e204032b3
yjh0410 <1394571815@qq.com>
2022-09-15 21:10:37 +0800

    KS = 9

--- config/yowof_config.py
+++ config/yowof_config.py
@@ -9,8 +9,8 @@ yowof_config = {
         'res5_dilation': False,
         'stride': 32,
         # temporal encoder
-        'conv_lstm_ks': 7,
-        'conv_lstm_pd': 3,
+        'conv_lstm_ks': 9,
+        'conv_lstm_pd': 4,
         'conv_lstm_di': 1,
         'conv_lstm_nl': 1,
         # head
