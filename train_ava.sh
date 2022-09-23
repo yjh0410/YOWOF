e0b6d6a02fc217c5bb3d17349270f1620ce679b7
yjh0410 <1394571815@qq.com>
2022-09-23 08:32:22 +0800

    update

--- train_ava.sh
+++ train_ava.sh
@@ -2,7 +2,7 @@
 python train.py \
         --cuda \
         -d ava_v2.2 \
-        -v yowof-r50-DC5 \
+        -v yowof-rx101 \
         --num_workers 8 \
         --eval_epoch 1 \
         --eval \
