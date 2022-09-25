fe0a0a9ca93df17c0aad36a9119fb026ebbe2dec
yjh0410 <1394571815@qq.com>
2022-09-25 15:07:06 +0800

    train YOWOF-R50-DC5 on ava

--- config/yowof_config.py
+++ config/yowof_config.py
@@ -82,6 +82,46 @@ yowof_config = {
                         [256, 256]], # 320
     },
 
+    'yowof-r50-DC5': {
+        # backbone
+        'backbone': 'resnet50',
+        'pretrained': True,
+        'res5_dilation': True,
+        'stride': 16,
+        # temporal encoder
+        'conv_lstm_ks': 3,
+        'conv_lstm_pd': 1,
+        'conv_lstm_di': 1,
+        'conv_lstm_nl': 1,
+        # head
+        'head_dim': 256,
+        'head_norm': 'BN',
+        'head_act': 'relu',
+        'num_cls_heads': 4,
+        'num_reg_heads': 4,
+        'head_depthwise': False,
+        # batch size
+        'batch_size': 8,
+        'accumulate': 16,
+        # matcher
+        'matcher': 'uniform_matcher',
+        'topk': 4,
+        'iou_t': 0.15,
+        'igt': 0.7,
+        'ctr_clamp': 32,
+        # loss
+        'alpha': 0.25,
+        'gamma': 2.0,
+        'loss_cls_weight': 1.0,
+        'loss_reg_weight': 1.0,
+        # anchor size
+        'anchor_size': [[16, 16],
+                        [32, 32],
+                        [64, 64],
+                        [128, 128],
+                        [256, 256]], # 320
+    },
+
     'yowof-rx101': {
         # backbone
         'backbone': 'resnext101',
