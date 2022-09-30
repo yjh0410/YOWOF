7c85212abe95b5d723c69a68dc879d7800b096b2
yjh0410 <1394571815@qq.com>
2022-09-30 10:37:07 +0800

    add FrozenBN

--- models/backbone/__init__.py
+++ models/backbone/__init__.py
@@ -7,7 +7,11 @@ def build_backbone(cfg, pretrained=False):
     print('--pretrained: {}'.format(pretrained))
 
     if cfg['backbone'] in ['resnet18', 'resnet50', 'resnet101', 'resnext101']:
-        model, feat_dim = build_resnet_2d(cfg['backbone'], pretrained, cfg['res5_dilation'])
+        model, feat_dim = build_resnet_2d(
+            model_name=cfg['backbone'],
+            pretrained=pretrained,
+            norm_layer=cfg['norm_layer'],
+            res5_dilation=cfg['res5_dilation'])
 
     else:
         print('Unknown Backbone ...')
